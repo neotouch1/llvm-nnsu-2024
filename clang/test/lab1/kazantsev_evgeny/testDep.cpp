0d7d629c9d9b89d4f72bd1e5d5db7fffb4e71b7d
neotouch1 <workgen@bk.ru>
2024-03-25 18:54:36 +0300

    added check class

--- clang/test/lab1/kazantsev_evgeny/testDep.cpp
+++ clang/test/lab1/kazantsev_evgeny/testDep.cpp
@@ -10,6 +10,11 @@ void dEpReCaTeD(int c);
 // DEPRECATED-NOT: warning: Deprecated function name
 void sqrt();
 
+// DEPRECATED: warning: Deprecated function name
+class A {
+  int method_deprecated();
+};
+
 // RUN: %clang_cc1 -load %llvmshlibdir/DeprecatedPlug%pluginext -plugin deprecated-function %s -plugin-arg-deprecated-function help 2>&1 | FileCheck %s --check-prefix=HELP
 // REQUIRES: plugins
 
